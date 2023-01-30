cc040cf27b8d3f2f60da900f99b0ffa2947f2746
Limhi Navarro <lp.nav95@gmail.com>
2023-01-30 11:56:20 -0600

    Se integra configuracion de CORS para funcionamiento con la aplicacion web

---
+++ src/main/java/com/nav/billing/appbillingv1/configuration/GlobalConfiguration.java
@@ -0,0 +1,31 @@
+package com.nav.billing.appbillingv1.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalConfiguration {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+
+    return new WebMvcConfigurer() {
+
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+
+        System.out.println("addCorsMappings...");
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://localhost:4200") // "ip" de servicio angular
+            .allowedMethods("*")//GET", "POST
+            .allowedHeaders("*")
+            .exposedHeaders("*");
+
+      }
+    };
+  }
+
+}
